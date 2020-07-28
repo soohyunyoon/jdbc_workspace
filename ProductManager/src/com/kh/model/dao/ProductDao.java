62880afc96ac3bdc51b0dbf652eb85403bfda573
soohyun yoon <shyuny58@google.com>
2020-07-29 02:24:28 +0900

    UPDATE

---
+++ ProductManager/src/com/kh/model/dao/ProductDao.java
@@ -0,0 +1,156 @@
+package com.kh.model.dao;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import com.kh.common.JDBCTemplate;
+import com.kh.model.vo.Product;
+
+public class ProductDao {
+
+	private Properties prop = new Properties();
+	
+	public ProductDao() {
+		try {
+			prop.load(new FileReader("resources/query.properties"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+			
+	public ArrayList<Product> selectList(Connection conn) {
+		ArrayList<Product> list = new ArrayList<>();
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("selectlist");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			while(rset.next()) {
+				list.add(new Product(rset.getString("PRODUCT_ID"),
+									rset.getString("P_NAME"),
+									rset.getInt("price"),
+									rset.getString("description"),
+									rset.getInt("stock")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+		return list;
+	}
+	
+	public int insertProduct(Connection conn, Product p) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String sql = prop.getProperty("insertproduct");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1,p.getProductId());
+			pstmt.setString(2, p.getP_Name());
+			pstmt.setInt(3, p.getPrice());
+			pstmt.setString(4, p.getDescription());
+			pstmt.setInt(5, p.getStock());
+			
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(pstmt);
+		}
+		
+		return result;
+	}
+	
+	public int updateProduct(Connection conn, Product p) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String sql = prop.getProperty("updateproduct");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, p.getP_Name());
+			pstmt.setInt(2, p.getPrice());
+			pstmt.setString(3, p.getDescription());
+			pstmt.setInt(4, p.getStock());
+			pstmt.setString(5, p.getProductId());
+			
+			result = pstmt.executeUpdate();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(pstmt);
+		}
+		
+		return result;
+	}
+	
+	public int deleteProduct(Connection conn, String pId) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String sql = prop.getProperty("deleteproduct");
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, pId);
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(pstmt);
+		}
+		return result;
+	}
+	
+	public ArrayList<Product> selectProduct(Connection conn, String keyword) {
+		ArrayList<Product> list = new ArrayList<>();
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		
+		String sql = prop.getProperty("selectproduct");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "%" + keyword + "%");
+			rset = pstmt.executeQuery();
+			while(rset.next()) {
+				list.add(new Product (rset.getString("product_id"),
+								rset.getString("P_Name"),
+								rset.getInt("price"),
+								rset.getString("description"),	
+								rset.getInt("stock")));
+
+				
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+		return list;
+	}
+}
